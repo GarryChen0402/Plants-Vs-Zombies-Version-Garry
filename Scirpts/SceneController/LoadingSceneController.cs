8ae5be88dd4d8a49ddfbab14f9b80f5f3a0bd905
GarryChen0402 <gychen0402@foxmail.com>
2025-11-28 15:05:27 +0800

    Scene: Create the loading scene

---
+++ Scirpts/SceneController/LoadingSceneController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Loading;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingSceneController : MonoBehaviour
+{
+    // Control the loading bar animation
+    public Image loadingBarGrassMask;
+    private const float loadingBarGrassMaskMaxWidth = 321;
+    private const float loadingBarGrassMaskMaxTime = 4f;
+    private float loadingBarGrassMaskSpeed;
+    private float currentLoadingBarGrassMaskWidth;
+
+    public Image loadingBarGrassRoll;
+    private const float loadingBarGrassRollStartPositionX = 205f;
+    private const float loadingBarGrassRollEndPositionX = 505f;
+    private const float loadingBarGrassRollPathLength = 300f;
+    private float loadingBarGrassRollSpeed;
+    private float currentLoadingBarGrassRollPositionX;
+
+    private bool isLoadingBarComplete = false;
+
+
+
+    private void Awake()
+    {
+        LoadingBarAwake();
+    }
+
+    private void Start()
+    {
+        LoadingBarStart();
+    }
+
+    private void Update()
+    {
+        LoadingBarUpdate();
+    }
+    private void LoadingBarAwake()
+    {
+        loadingBarGrassMaskSpeed = loadingBarGrassMaskMaxWidth / loadingBarGrassMaskMaxTime;
+        currentLoadingBarGrassMaskWidth = 0f;
+        loadingBarGrassRollSpeed = loadingBarGrassRollPathLength / loadingBarGrassMaskMaxTime;
+        currentLoadingBarGrassRollPositionX = loadingBarGrassRollStartPositionX;
+        isLoadingBarComplete = false;
+    }
+
+    private void LoadingBarStart()
+    {
+        SetMaskWidth(0);
+        SetRollPosX(currentLoadingBarGrassRollPositionX);
+    }
+
+    private void LoadingBarUpdate()
+    {
+        currentLoadingBarGrassMaskWidth = math.min(currentLoadingBarGrassMaskWidth + loadingBarGrassMaskSpeed * Time.deltaTime, loadingBarGrassMaskMaxWidth);
+        currentLoadingBarGrassRollPositionX = math.min(currentLoadingBarGrassRollPositionX + loadingBarGrassRollSpeed * Time.deltaTime, loadingBarGrassRollEndPositionX);
+        SetMaskWidth(currentLoadingBarGrassMaskWidth);
+        SetRollPosX(currentLoadingBarGrassRollPositionX);
+        if(!isLoadingBarComplete && currentLoadingBarGrassRollPositionX == loadingBarGrassRollEndPositionX && currentLoadingBarGrassMaskWidth == loadingBarGrassMaskMaxWidth)
+        {
+            isLoadingBarComplete = true;
+            SetLoadingBarGrassRollInvisible();
+        }
+    }
+
+    private void SetMaskWidth(float width)
+    {
+        loadingBarGrassMask.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
+    }
+
+    private void SetRollPosX(float x)
+    {
+        Vector3 position = loadingBarGrassRoll.transform.position;
+        position.x = x;
+        loadingBarGrassRoll.GetComponent<RectTransform>().SetPositionAndRotation(position, Quaternion.identity);
+    }
+
+    private void SetLoadingBarGrassRollInvisible()
+    {
+        loadingBarGrassRoll.enabled = false;
+    }
+
+}
